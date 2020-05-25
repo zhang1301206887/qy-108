1208d94e35e4afd5b15b6bf98a24158043dd3082
hk-xiao <64893760+hk-xiao@users.noreply.github.com>
2020-05-25 21:05:16 +0800

    hk

--- qy-108-management/qy-108-mapper/src/main/java/com/aaa/zk/mapper/RoleMenuMapper.java
+++ qy-108-management/qy-108-mapper/src/main/java/com/aaa/zk/mapper/RoleMenuMapper.java
@@ -1,18 +1,16 @@
 package com.aaa.zk.mapper;
 
 import com.aaa.zk.model.RoleMenu;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
 public interface RoleMenuMapper extends Mapper<RoleMenu> {
 
-    @Select("select * from t_role_menu where role_id = #{roleId}")
+
     List<RoleMenu> selectRoleMenuById(Object id);
 
-    @Delete("delete from t_role_menu where role_id = #{roleId}")
+
     Integer deleteByRoleId(Object roleId);
 
 }
