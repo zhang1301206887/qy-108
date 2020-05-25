d0d4e84fb7de8b13b1dd850c6abf157ec9b63887
hk-xiao <64893760+hk-xiao@users.noreply.github.com>
2020-05-25 21:04:32 +0800

    hk

--- qy-108-management/qy-108-mapper/src/main/java/com/aaa/zk/mapper/MenuMapper.java
+++ qy-108-management/qy-108-mapper/src/main/java/com/aaa/zk/mapper/MenuMapper.java
@@ -3,14 +3,12 @@ package com.aaa.zk.mapper;
 
 import com.aaa.zk.model.Menu;
 import com.aaa.zk.vo.MenuVo;
-import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
 public interface MenuMapper extends Mapper<Menu> {
 
-    @Select("select * from t_menu where parent_id = #{parent_id}")
     List<MenuVo> selectMenuByParentId(Object parentId);
 
 }
--- qy-108-management/qy-108-mapper/src/main/java/com/aaa/zk/mapper/TechnicistMapper.java
+++ qy-108-management/qy-108-mapper/src/main/java/com/aaa/zk/mapper/TechnicistMapper.java
@@ -1,14 +1,12 @@
 package com.aaa.zk.mapper;
 
 import com.aaa.zk.model.Technicist;
-import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
 public interface TechnicistMapper extends Mapper<Technicist> {
 
-    @Select("select * from t_technicist where user_id = #{userid}")
-    public List<Technicist> selectTechByUserId(Object userId);
+    List<Technicist> selectTechByUserId(Object userId);
 
 }
