fc634ec1150bcb46fa412379e6957f3b78baafb0
hk-xiao <64893760+hk-xiao@users.noreply.github.com>
2020-06-01 14:55:03 +0800

    hk

--- qy-108-provider-management/qy-108-system-provider-6081/src/main/java/com/aaa/zk/ApplicationRun8081.java
+++ qy-108-provider-management/qy-108-system-provider-6081/src/main/java/com/aaa/zk/ApplicationRun8081.java
@@ -3,6 +3,7 @@ package com.aaa.zk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
@@ -17,15 +18,14 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication(exclude = {
         RedisAutoConfiguration.class,
-        RedisRepositoriesAutoConfiguration.class
+        RedisRepositoriesAutoConfiguration.class,
+        DataSourceAutoConfiguration.class
 })
 @MapperScan("com.aaa.zk.mapper")
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 public class ApplicationRun8081 {
-
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRun8081.class, args);
     }
-
 }
