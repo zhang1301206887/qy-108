dad5751b67d4afb5f99b99298cf2b89893a86d59
hk-xiao <64893760+hk-xiao@users.noreply.github.com>
2020-06-05 10:37:17 +0800

    hk

--- qy-108-consumer-management/qy-108-sytem-consumer-6081/src/main/java/com/aaa/zk/controller/UploadController.java
+++ qy-108-consumer-management/qy-108-sytem-consumer-6081/src/main/java/com/aaa/zk/controller/UploadController.java
@@ -9,6 +9,7 @@ import com.aaa.zk.base.BaseController;
 import com.aaa.zk.base.ResultData;
 import com.aaa.zk.service.IQYUploadService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +21,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @Api(value = "文件上传", tags = "文件上传的接口")
 public class UploadController extends BaseController {
+
     @Autowired
     IQYUploadService uploadService;
+
     @PostMapping("/uploadFile")
+    @ApiOperation(value = "上传功能",notes = "单个文件上传")
     public ResultData uploadFile(@RequestBody MultipartFile file , @RequestParam("memo") String memo
             ,@RequestParam("ref_biz_type") String ref_biz_type , @RequestParam("ref_biz_id") Long ref_biz_id){
         return uploadService.uploadFile(file, memo, ref_biz_type, ref_biz_id);
